8910daae2e72ce1675bc19071693394a74bc11f6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-11-08 21:42:42 -0300

    fix: rename files

--- src/data/use-cases/remote-account.ts
+++ src/data/use-cases/remote-account.ts
@@ -1,12 +1,15 @@
+import { InvalidCredentialsError } from "../../domain/error/invalid-credentials-error";
 import { SameEmailError } from "../../domain/error/same-email-error";
 import { User } from "../../domain/models";
 import { CreateAccount, RequestCreateAccount } from "../../domain/use-cases";
 import { LoginAccount, RequestLoginAccount, ResponseLoginAccount } from "../../domain/use-cases/login-account";
 import { BdClient } from "../protocols/bd";
+import { Token } from "../protocols/token";
 
 export class RemoteAccount implements CreateAccount, LoginAccount {
     constructor(
-        private bdClient: BdClient
+        private bdClient: BdClient,
+        private token: Token
     ) { };
 
     async createAccount(params: RequestCreateAccount): Promise<void> {
@@ -31,6 +34,19 @@ export class RemoteAccount implements CreateAccount, LoginAccount {
     };
 
     async loginAccount(params: RequestLoginAccount): Promise<ResponseLoginAccount> {
-        throw new Error("Method not implemented.");
+        const haveUser = await this.bdClient.getModel<RequestLoginAccount, boolean>({
+            model: 'User',
+            body: {
+                email: params.email,
+                password: params.password
+            }
+        });
+
+        if (!haveUser)
+            throw new InvalidCredentialsError();
+
+        return this.token.generate({
+            email: params.email
+        });
     };
 }
