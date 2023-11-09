70787715603f3b442e581ced4404184471f1f4f8
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-11-08 21:44:14 -0300

    test: create functions for tests

---
+++ test/domain/mocks/remote-account.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+import { RequestCreateAccount } from "../../../src/domain/use-cases";
+import { RequestLoginAccount } from "../../../src/domain/use-cases/login-account";
+
+export const requestCreateAccount = (props?: Partial<RequestCreateAccount>): RequestCreateAccount => {
+    return {
+        email: props?.email ?? faker.internet.email(),
+        password: props?.password ?? faker.word.words(),
+        username: props?.username ?? faker.name.firstName()
+    }
+};
+
+export const requestLoginAccount = (props: Partial<RequestLoginAccount>): RequestLoginAccount => {
+    return {
+        email: props?.email ?? faker.internet.email(),
+        password: props?.password ?? faker.word.words()
+    }
+};
