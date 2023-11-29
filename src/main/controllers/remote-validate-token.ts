ca879e7247df20b98a957f0d27b0c4cf46532113
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-11-29 18:37:53 -0300

    feat: validateTokenController

---
+++ src/main/controllers/remote-validate-token.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { InvalidCredentialsError } from "../../domain/error/invalid-credentials-error";
+import { makeRemoteValidateToken } from "../factories/use-cases/remote-validate-token";
+
+const validate = makeRemoteValidateToken();
+
+export const validateToken = async (req: FastifyRequest, rep: FastifyReply) => {
+    const { authorization } = req.headers;
+
+    try {
+        const token = await validate.validate(authorization.replace('Bearer ', ''));
+
+        rep.statusCode = 200;
+        rep.send(token);
+    } catch (error: any) {
+        rep.statusCode = error instanceof InvalidCredentialsError ?
+            401 : 500;
+
+        rep.send(error.message);
+    }
+}
