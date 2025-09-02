fcd1a66ba6e5af33f92cc33d2a133f77d4b9ba34
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-02 17:29:19 -0300

    fix: makeRemoteAccount add makeRefreshToken

--- src/main/factories/use-cases/remote-account.ts
+++ src/main/factories/use-cases/remote-account.ts
@@ -4,10 +4,12 @@ import { makeBdPrimaClient } from "../bdClient/bd-prisma-client";
 import { makeCryptoEncrypt } from "../encrypt/crypto-encrypt";
 import { makeUuidGuid } from "../guid/uuid-guid-factory";
 import { makeJsonWebToken } from "../token/json-web-token";
+import { makeRefreshToken } from "./refresh-token";
 
 export const makeRemoteAccount = (): CreateAccount & LoginAccount => new RemoteAccount(
     makeBdPrimaClient(),
     makeJsonWebToken(),
     makeCryptoEncrypt(),
-    makeUuidGuid()
+    makeUuidGuid(),
+    makeRefreshToken()
 );
