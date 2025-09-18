943c4f55f2d0f266dd6408179e0801511c970825
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-17 22:01:57 -0300

    test: createUser function

---
+++ test/domain/mocks/user.ts
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+import { User } from "../../../src/domain/models";
+
+export const createUser = (params?: Partial<User>): User => ({
+    id: params?.id ?? faker.string.uuid(),
+    email: params?.email ?? faker.internet.email(),
+    password: params?.password ?? faker.internet.password(),
+    username: params?.username ?? faker.internet.userName(),
+    refreshToken: params?.refreshToken ?? faker.string.uuid()
+});
