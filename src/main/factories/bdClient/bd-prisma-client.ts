04d7c1c1025a718f84d6fecac92d7987b5abcb20
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-23 21:42:38 -0300

    feat: remove prisma files

--- src/main/factories/bdClient/bd-prisma-client.ts
+++
@@ -1,4 +0,0 @@
-import { BdClient } from "../../../data/protocols/bd";
-import { BdPrismaClient } from "../../../infra/bdClient/bd-prisma-client";
-
-export const makeBdPrimaClient = (): BdClient => new BdPrismaClient();
