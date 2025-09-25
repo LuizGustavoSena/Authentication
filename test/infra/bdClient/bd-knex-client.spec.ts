4afc34153599889580c60a941f53d320c240e3f4
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-09-25 17:19:42 -0300

    test: BdKnexClient - should be successful create a new user

--- test/infra/bdClient/bd-knex-client.spec.ts
+++ test/infra/bdClient/bd-knex-client.spec.ts
@@ -1,10 +1,20 @@
 import { describe, expect, it } from 'vitest';
-import { knex } from '../../../src/infra/bdClient/knex/database';
+import { KnexBdClient } from '../../../src/infra/bdClient/knex/knex-bd-client';
+import { requestCreateAccount } from '../../domain/mocks/remote-account';
+
+const makeSut = () => new KnexBdClient();
 
 describe('BdKnexClient', () => {
-    it('should be', async () => {
-        const test = await knex('sqlite_schema').select('*');
+    it('should be successful create a new user', async () => {
+        const sut = makeSut();
 
-        expect(test).toHaveLength(0);
+        const request = requestCreateAccount();
+
+        const test = await sut.createUser(request);
+
+        expect(test).not.toHaveProperty('password');
+        expect(test.id).toBe(request.id);
+        expect(test.email).toBe(request.email);
+        expect(test.username).toBe(request.username);
     });
-})
+});
