7e7e4a5676ada050cdc967d1c9f05715edd64d71
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-04 19:10:03 -0300

    fix: alter env settings

--- test/data/use-cases/remote-validate-token.spec.ts
+++ test/data/use-cases/remote-validate-token.spec.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker';
 import { describe, expect, it } from 'vitest';
 import { RemoteValidateToken } from "../../../src/data/use-cases/remote-validate-token";
 import { InvalidCredentialsError } from '../../../src/domain/error/invalid-credentials-error';
+import { env } from '../../../src/infra/zod/env';
 import { TokenSpy } from "../mocks/mock-token";
 
 type Props = {
@@ -31,7 +32,7 @@ describe('RemoteValidateToken', () => {
         const response = sut.validate(userId);
 
         expect(response.userId).toBe(userId);
-        expect(response.expires).toBe(response.issued + Number(process.env.EXPIRES_TOKEN_MILLISECONDS));
+        expect(response.expires).toBe(response.issued + env.EXPIRES_TOKEN_MILLISECONDS);
     });
 
     it('Should credetials invalid token', () => {
