8f8cefa9da05656d5f2bbd27329a4661ddda710e
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-05 17:09:06 -0300

    fix: makeBdClient in makeRemoteAccount

--- src/main/factories/use-cases/remote-account.ts
+++ src/main/factories/use-cases/remote-account.ts
@@ -1,13 +1,13 @@
 import { RemoteAccount } from "../../../data/use-cases/remote-account";
 import { CreateAccount, LoginAccount } from "../../../domain/use-cases";
-import { makeBdPrimaClient } from "../bdClient/bd-prisma-client";
+import { makeBdClient } from "../database";
 import { makeCryptoEncrypt } from "../encrypt/crypto-encrypt";
 import { makeUuidGuid } from "../guid/uuid-guid-factory";
 import { makeJsonWebToken } from "../token/json-web-token";
 import { makeRefreshToken } from "./refresh-token";
 
 export const makeRemoteAccount = (): CreateAccount & LoginAccount => new RemoteAccount(
-    makeBdPrimaClient(),
+    makeBdClient(),
     makeJsonWebToken(),
     makeCryptoEncrypt(),
     makeUuidGuid(),
