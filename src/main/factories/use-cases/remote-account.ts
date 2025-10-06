a884d8ab12e02e8d5d8ae3781b9d9d93893ea9c8
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-06 16:53:35 -0300

    fix: alter make functions

--- src/main/factories/use-cases/remote-account.ts
+++ src/main/factories/use-cases/remote-account.ts
@@ -1,15 +1,15 @@
 import { RemoteAccount } from "../../../data/use-cases/remote-account";
 import { CreateAccount, LoginAccount } from "../../../domain/use-cases";
+import { makeCrypto } from "../crypt";
 import { makeBdClient } from "../database";
-import { makeCryptoEncrypt } from "../encrypt/crypto-encrypt";
-import { makeUuidGuid } from "../guid/uuid-guid-factory";
-import { makeJsonWebToken } from "../token/json-web-token";
+import { makeGuid } from "../guid";
+import { makeToken } from "../json-web-token";
 import { makeRefreshToken } from "./refresh-token";
 
 export const makeRemoteAccount = (): CreateAccount & LoginAccount => new RemoteAccount(
     makeBdClient(),
-    makeJsonWebToken(),
-    makeCryptoEncrypt(),
-    makeUuidGuid(),
+    makeToken(),
+    makeCrypto(),
+    makeGuid(),
     makeRefreshToken()
 );
