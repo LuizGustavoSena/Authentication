014d387c05ba0ae2b9d8bbf580d234c5caa33433
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-16 17:03:50 -0300

    fix: alter validateToken to class controler

--- src/main/controllers/remote-validate-token.ts
+++ src/main/controllers/remote-validate-token.ts
@@ -1,35 +1,40 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { ZodError, z } from "zod";
+import { ZodError } from "zod";
 import { InvalidCredentialsError } from "../../domain/error/invalid-credentials-error";
-import { makeRemoteValidateToken } from "../factories/use-cases/remote-validate-token";
+import { ValidateToken } from "../../domain/use-cases";
+import { RemoteValidateTokenValidation } from "../../domain/validations/remote-validate-token-validation";
 
-const validate = makeRemoteValidateToken();
+export default class ValidateTokenController {
+    constructor(
+        private validation: RemoteValidateTokenValidation,
+        private remoteValidation: ValidateToken
+    ) { };
 
-const validationValidateToken = z.string();
+    async validateToken(req: FastifyRequest, rep: FastifyReply) {
+        const { authorization } = req.headers;
 
-export const validateToken = async (req: FastifyRequest, rep: FastifyReply) => {
-    const { authorization } = req.headers;
+        try {
+            this.validation.validateToken(authorization);
 
-    try {
-        validationValidateToken.parse(authorization);
+            const token = this.remoteValidation.validate(authorization.replace('Bearer ', ''));
 
-        const token = await validate.validate(authorization.replace('Bearer ', ''));
+            rep.statusCode = 200;
+            rep.send(token);
+        } catch (error: any) {
+            let code = 500;
+            let message = 'Erro inesperado';
 
-        rep.statusCode = 200;
-        rep.send(token);
-    } catch (error: any) {
-        let code = 500;
-        let message = 'Erro inesperado';
+            if (error instanceof InvalidCredentialsError || error instanceof ZodError) {
+                code = error instanceof ZodError ? 415 : 401;
+                message = error.message;
+            };
 
-        if (error instanceof InvalidCredentialsError || error instanceof ZodError) {
-            code = error instanceof ZodError ? 415 : 401;
-            message = error.message;
-        };
+            if (!(error instanceof InvalidCredentialsError) && !(error instanceof ZodError))
+                req.log.info(error.message);
 
-        if (!(error instanceof InvalidCredentialsError) && !(error instanceof ZodError))
-            req.log.info(error.message);
-
-        rep.statusCode = code;
-        rep.send(message);
+            rep.statusCode = code;
+            rep.send(message);
+        }
     }
 }
+
