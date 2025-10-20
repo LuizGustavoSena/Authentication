0ee5d7459afd84ca99517f3a0ff8d13690f185f6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-20 16:57:30 -0300

    test: RemoteAccountValidationZod -Should be successfull create account

--- test/infra/validation/remote-account-validation-zod.spec.ts
+++ test/infra/validation/remote-account-validation-zod.spec.ts
@@ -1,9 +1,16 @@
-import { describe } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import RemoteAccountValidationZod from "../../../src/infra/zod/remote-account-validation-zod";
+import { requestCreateAccount } from '../../domain/mocks/remote-account';
 
 export const makeSut = (): RemoteAccountValidationZod =>
     new RemoteAccountValidationZod();
 
 describe('RemoteAccountValidationZod', () => {
+    it('Should be successfull create account', () => {
+        const sut = makeSut();
 
+        const request = requestCreateAccount();
+
+        expect(() => sut.createAccount(request)).resolves.toBeUndefined();
+    });
 });
