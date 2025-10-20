8d138f5ad91edf00a2ba0152b02bb17302a0fc4f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-20 16:51:34 -0300

    fix: function type makeRemoteValidateTokenControler

--- src/main/factories/controllers/remote-validate-token.ts
+++ src/main/factories/controllers/remote-validate-token.ts
@@ -2,7 +2,7 @@ import ValidateTokenController from "../../controllers/remote-validate-token";
 import { makeRemoteValidateToken } from "../use-cases/remote-validate-token";
 import { makeRemoteValidateTokenValidation } from "../validations/remote-validate-token";
 
-export const makeRemoteValidateTokenControler = () => new ValidateTokenController(
+export const makeRemoteValidateTokenControler = (): ValidateTokenController => new ValidateTokenController(
     makeRemoteValidateTokenValidation(),
     makeRemoteValidateToken()
 );
