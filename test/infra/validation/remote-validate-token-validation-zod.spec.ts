b35afbdf8730ee6ba468349906f431e92e793a79
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-29 17:02:47 -0300

    test: RemoteValidateTokenValidationZod

---
+++ test/infra/validation/remote-validate-token-validation-zod.spec.ts
@@ -0,0 +1,8 @@
+import RemoteValidateTokenValidationZod from "../../../src/infra/zod/remote-validate-token-validation-zod";
+
+export const makeSut = (): RemoteValidateTokenValidationZod =>
+    new RemoteValidateTokenValidationZod();
+
+describe('RemoteValidateTokenValidationZod', () => {
+
+});
