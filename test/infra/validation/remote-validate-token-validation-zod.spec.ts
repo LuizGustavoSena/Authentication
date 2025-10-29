99dcaf105d53225c8356a49adade8eac669cc614
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-10-29 17:05:41 -0300

    test: RemoteValidateTokenValidationZod - Should be successfull validate token

--- test/infra/validation/remote-validate-token-validation-zod.spec.ts
+++ test/infra/validation/remote-validate-token-validation-zod.spec.ts
@@ -1,8 +1,15 @@
+import { faker } from "@faker-js/faker";
+import { describe, expect, it } from 'vitest';
+import { ValidationError } from "../../../src/domain/error/validation-error";
 import RemoteValidateTokenValidationZod from "../../../src/infra/zod/remote-validate-token-validation-zod";
 
 export const makeSut = (): RemoteValidateTokenValidationZod =>
     new RemoteValidateTokenValidationZod();
 
 describe('RemoteValidateTokenValidationZod', () => {
+    it('Should be successfull validate token', () => {
+        const sut = makeSut();
 
+        expect(() => sut.validateToken(faker.string.uuid())).not.toThrow(ValidationError);
+    });
 });
