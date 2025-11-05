b625188cb510e0481ed902e7bfa7f54a767e61fe
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-11-05 16:58:28 -0300

    feat: auth route app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import AuthRouter from './routes/auth-route';
+
 const app = express();
 const protectedRouter = express.Router();
 
 app.use(express.json());
+
+protectedRouter.use('/auth', AuthRouter);
 
 export default app;
