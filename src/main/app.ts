179fb8afacc0c17b7cf372ec4cad20779921b27f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-11-05 16:59:03 -0300

    feat: token route app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import AuthRouter from './routes/auth-route';
+import TokenRouter from './routes/token-route';
 
 const app = express();
 const protectedRouter = express.Router();
@@ -7,5 +8,6 @@ const protectedRouter = express.Router();
 app.use(express.json());
 
 protectedRouter.use('/auth', AuthRouter);
+protectedRouter.use('/auth', TokenRouter);
 
 export default app;
