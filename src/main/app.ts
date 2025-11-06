8a5a261c1d524d63ae3e463dc2402a03cc85a41a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-11-06 16:59:54 -0300

    feat: implement routers to express app

--- src/main/app.ts
+++ src/main/app.ts
@@ -3,11 +3,10 @@ import AuthRouter from './routes/auth-route';
 import TokenRouter from './routes/token-route';
 
 const app = express();
-const protectedRouter = express.Router();
 
 app.use(express.json());
 
-protectedRouter.use('/auth', AuthRouter);
-protectedRouter.use('/auth', TokenRouter);
+app.use('/auth', AuthRouter);
+app.use('/auth', TokenRouter);
 
 export default app;
