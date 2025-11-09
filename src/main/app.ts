3767b421cac5dc9f5f93ab0ca7e1b92d149730c5
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-11-09 14:22:31 -0300

    feat: implement helmet in express app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
 import AuthRouter from './routes/auth-route';
 import TokenRouter from './routes/token-route';
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 
 app.use('/auth', AuthRouter);
 app.use('/auth', TokenRouter);
