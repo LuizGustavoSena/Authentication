80b59f6e21026770bcc7e8eb76d8e4869ade9107
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-11-12 17:07:01 -0300

    feat: rateLimit in express app

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import { rateLimiter } from './middlewares/rate-limit';
 import { tooBusyCheck } from './middlewares/too-busy-check';
 import AuthRouter from './routes/auth-route';
 import TokenRouter from './routes/token-route';
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(tooBusyCheck);
+app.use(rateLimiter);
 
 app.use('/auth', AuthRouter);
 app.use('/auth', TokenRouter);
